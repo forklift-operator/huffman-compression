c3bf92871210f15b08391ed77bc6a0a105478c1d
Stoyan Yordanov <137334856+forklift-operator@users.noreply.github.com>
2024-12-29 17:24:11 +0200

    Fixed the min-heap

--- minHeap.cpp
+++ minHeap.cpp
@@ -2,114 +2,100 @@
 #include <vector>
 #include <algorithm>
 
-template <typename T>
+template <typename T, typename Comparator = std::less<T>>
 class minHeap
 {
 private:
-    int size;
-    int cap;
-    std::vector<T> heap;
+    std::vector<T> data;
+    Comparator comp;
 
     int parent(int index) { return (index - 1) / 2; };
     int left(int index) { return (index * 2) + 1; };
     int right(int index) { return (index * 2) + 2; };
 
 public:
-    minHeap(int cap);
+    minHeap(){};
+    minHeap(size_t size) {data.reserve(size);};
+    minHeap(const std::vector<int>& other) {buildHeap(other);};
 
+    void buildHeap(const std::vector<int>& other);
     void insert(T val);
-    int extractMin();
+    T extractMin();
     void soak(int index);
     void heapify(int index);
     void print() const;
+    size_t size() const;
+    bool empty() const;
 
     ~minHeap() {};
 };
 
-template <typename T>
-minHeap<T>::minHeap(int cap)
+template <typename T, typename Comparator>
+void minHeap<T, Comparator>::buildHeap(const std::vector<int> &other)
 {
-    size = 0;
-    this->cap = cap;
-    heap.resize(cap);
+    data = other;
+    for(int i = data.size()/2-1; i>=0;i--)
+        soak(i);
 }
 
-template <typename T>
-void minHeap<T>::insert(T val)
+template <typename T, typename Comparator>
+void minHeap<T, Comparator>::insert(T el)
 {
-    if (size == cap)
-    {
-        std::cout << "MINHEAP FULL!" << std::endl; // make this throw an error
-        return;
-    }
-
-    size++;
-
-    int i = size - 1;
-    heap[i] = val;
-    soak(i);
-    }
-
-template <typename T>
-int minHeap<T>::extractMin()
-{
-    if (size == 0)
-    {
-        std::cout << "MINHEAP IS EMPTY" << std::endl;
-        return -1;
-    }
-    else if (size == 1)
-    {
-        size--;
-        return heap[0];
-    }
-    else
-    {
-        int root = heap[0];
-
-        heap[0] = heap[size - 1];
-        size--;
-        heapify(0);
-
-        return root;
-    }
+    data.push_back(el);
+    heapify(data.size()-1);
 }
 
-template <typename T>
-void minHeap<T>::soak(int index)
+template <typename T, typename Comparator>
+T minHeap<T, Comparator>::extractMin()
 {
-    while (index != 0 && heap[parent(index)] > heap[index])
+    if (data.empty())
+        throw std::underflow_error("Heap is empty");
+    
+    T root = data[0];
+    data[0] = data[data.size() - 1];
+    data.pop_back();
+    
+    soak(0);
+
+    return root;
+}
+
+template <typename T, typename Comparator>
+void minHeap<T, Comparator>::heapify(int index)
+{
+    while (index != 0 && comp(data[index], data[parent(index)]))
     {
-        std::swap(heap[index], heap[parent(index)]);
+        std::swap(data[index], data[parent(index)]);
         index = parent(index);
     }
 }
 
-template <typename T>
-void minHeap<T>::heapify(int index)
+
+template <typename T, typename Comparator>
+void minHeap<T, Comparator>::soak(int index)
 {
     int l = left(index);
     int r = right(index);
     int smallest = index;
 
-    if ((l < size) && (heap[l] < heap[smallest]))
+    if ((l < data.size()) && comp(data[l], data[smallest]))
         smallest = l;
-    if ((r < size) && (heap[r] < heap[smallest]))
+    if ((r < data.size()) && comp(data[r], data[smallest]))
         smallest = r;
 
     if (smallest != index)
     {
-        std::swap(heap[index], heap[smallest]);
-        heapify(smallest);
+        std::swap(data[index], data[smallest]);
+        soak(smallest);
     }
 }
 
-template <typename T>
-void minHeap<T>::print() const
+template <typename T, typename Comparator>
+void minHeap<T, Comparator>::print() const
 {
     int pow = 0;
     int val = 1;
-    for (size_t i = 0; i < size; i++)
+    for (size_t i = 0; i < data.size(); i++)
     {
         if (i == val)
         {
@@ -117,24 +103,35 @@ void minHeap<T>::print() const
             pow++;
             val += (1 << pow);
         }
-        std::cout << heap[i] << " ";
+        std::cout << data[i] << " ";
     }
     std::cout << std::endl;
 }
 
-int main()
+template <typename T, typename Comparator>
+size_t minHeap<T, Comparator>::size() const
 {
-    int N = 15;
-
-    minHeap<int> heap(N);
-
-    for (size_t i = 0; i < N; i++)
-    {
-        char randomChar = 'A' + std::rand() % 26;
-        heap.insert(randomChar);
-        heap.print();
-        std::cout << std::endl;
-    }
-
-    return 0;
+    return data.size();
 }
+
+template <typename T, typename Comparator>
+bool minHeap<T, Comparator>::empty() const
+{
+    return data.empty();
+}
+
+// int main()
+// {
+//     int N = 15;
+
+//     minHeap<int> data(N);
+
+//     for (size_t i = 0; i < N; i++)
+//     {
+//         data.insert(std::rand() % 100);
+//         data.print();
+//         std::cout << std::endl;
+//     }
+
+//     return 0;
+// }
